a328cfefd8ae07784eaec50bbfc0cf325f8e9664
anzelo1 <36094317+anzelo1@users.noreply.github.com>
2018-09-09 15:59:05 -0500

    Solo toqué la clase Arreglos

--- QuickSort_Experiment/QuickSort_Experiment/Arreglos.cs
+++ QuickSort_Experiment/QuickSort_Experiment/Arreglos.cs
@@ -6,7 +6,75 @@ using System.Threading.Tasks;
 
 namespace QuickSort_Experiment
 {
-    class Arreglos
+    class Arreglo
     {
+
+        private int[] arregloEsperado { get; set; }
+        private Random random;
+
+        public int[] ArregloDesorganizado(int valor)
+        {
+            random = new Random();
+            for (int i = 0; i <= valor; i++)
+            {
+                arregloEsperado[i] = random.Next(1, 1000000000);
+            }
+
+            return arregloEsperado;
+        }
+
+        public int[] ArregloAscendente(int valor)
+        {
+            int num0 = 0;
+            int num1 = 1;
+            bool primer = false;
+            random = new Random();
+            for (int i = 0; i <= valor; i++)
+            {
+                num0 = random.Next(1, 1000000000);
+                num1 = random.Next(1, 1000000000);
+
+                if (primer == false)
+                {
+                    arregloEsperado[i] = num0;
+                    primer = true;
+                }
+                else
+                {
+
+
+                    if (num0 <= num1)
+                    {
+                        arregloEsperado[i] = num0;
+                    }
+                    else if (num1 < num0)
+                    {
+                        arregloEsperado[i] = num1;
+                    }
+                }
+            }
+
+            return arregloEsperado;
+        }
+
+        public int[] ArregloDescendente(int valor)
+        {
+            random = new Random();
+            for (int i = 0; i <= valor; i++)
+            {
+                int aleatorio = random.Next(1, 1000000000);
+                int num0 = random.Next(1, 250000000);
+                int num1 = random.Next(num0, 500000000);
+                int num2 = random.Next(num1, 750000000);
+                int num3 = random.Next(num2, 1000000000); ;
+                if (num0 < num1)
+                {
+
+                    arregloEsperado[i] = aleatorio;
+                }
+
+            }
+            return arregloEsperado;
+        }
     }
 }
